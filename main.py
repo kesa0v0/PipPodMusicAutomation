598d9182eb876e24e6b328d0aa1e168bbcfbc8ca
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-09-14 21:05:33 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from mainwnd_frontend import MainWnd
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MainWnd(root)
+    app.mainloop()
---
+++ mainwnd_frontend.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+
+
+class MainWnd(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        master.title("번호 자동으로 매기기 + .wav로 전환")
+        # master.geometry(0, 0)
+        master.resizable(False, False)
+        self.master = master
+
+        ttk.Label(self, text="위치:").grid(column=0, row=0)
+        self.path = tk.StringVar()
+        self.path_entered = ttk.Entry(self, width=60, textvariable=self.path)
+        self.path_entered.grid(column=1, row=0)
+
+        self.pathfinder = ttk.Button(self, text="열기", command=self.pathfinding)
+        self.pathfinder.grid(column=2, row=0)
+
+        self.ok = ttk.Button(self, text="확인", command=self.process)
+        self.ok.grid(column=0, row=1)
+        self.cancel = ttk.Button(self, text="취소", command=self.close)
+        self.cancel.grid(column=1, row=1)
+
+        self.pack()
+
+    def pathfinding(self):
+        filename = filedialog.askdirectory()
+        self.path.set(filename)
+
+    def close(self):
+        self.quit()
+
+    def process(self):
+        pass
+
